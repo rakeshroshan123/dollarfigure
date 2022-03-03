23cd62a25299853239c2e003416da7df8608e38b
rakeshroshan123 <100143527+rakeshroshan123@users.noreply.github.com>
2022-03-03 21:35:07 +0530

    sample programs

---
+++ src/dollarfigure/javapractice.java
@@ -0,0 +1,27 @@
+package dollarfigure;
+
+public class javapractice {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <= 7; i++) {
+            for(int j = 0; j < 2 * i - 2; j++)
+                System.out.print("*");
+                
+            for(int j = 0; j < -i + 8; j++)
+                System.out.print("$");
+                
+            for(int j = 0; j < -2 * i + 16; j++)
+                System.out.print("*");
+                
+            for(int j = 0; j < -i + 8; j++)
+                System.out.print("$");
+                
+            for(int j = 0; j < 2 * i - 2; j++)
+                System.out.print("*");
+                
+            System.out.println();
+        }
+    }
+}
+
+	
