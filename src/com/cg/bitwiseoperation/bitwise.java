412f8ed7b761a340a1d9542bf7f6c4ff81b323b8
rakeshroshan123 <100143527+rakeshroshan123@users.noreply.github.com>
2022-03-03 23:43:14 +0530

    bit wise examples

---
+++ src/com/cg/bitwiseoperation/bitwise.java
@@ -0,0 +1,24 @@
+package com.cg.bitwiseoperation;
+
+public class bitwise {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			int a=8;
+			int b=7;
+			System.out.println("A & B = "+(a&b));
+			System.out.println("A | B = "+(a^b));
+			System.out.println("A ^ B = "+(a^b));
+			System.out.println("~ A = "+(~ a));
+			System.out.println("~ B = "+(~ b));
+			System.out.println("A >> B = "+(a>>b));
+			System.out.println("A << B = "+(a<<b));
+			System.out.println("A >>> B = "+(a>>>b));
+		
+			
+		}
+
+	}
+
---
+++ src/com/cg/bitwiseoperation/example2.java
@@ -0,0 +1,13 @@
+package com.cg.bitwiseoperation;
+
+public class example2 {
+	
+		public static void main(String[] args) {
+			int a=1;
+			String result;
+			result = (a>=9) ? 
+					"a is greater than 9" : "a is less tahn 9";
+			System.out.println(result);
+		}
+
+	}
